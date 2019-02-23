aa0ca13203013fd6876aa57be667bd40b3ed520c
Тынышбаев Алибек Алмасулы <@corp.itstep>
2019-02-23 15:20:53 +0600

    homework module 8

---
+++ homework_module08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_module08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало диапазона массива");
+            int lowerIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конец диапазона массива");
+            int upperIndex = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (!(lowerIndex > upperIndex))
+            {
+                Console.WriteLine("Нижний индекс равен " + lowerIndex + " верхний индекс равен " + upperIndex);
+
+                RangeOfArray array = new RangeOfArray(upperIndex, lowerIndex);
+                for (int i = 0; i < array._arr.Length; i++)
+                {
+                    array._arr[i] = lowerIndex;
+                }
+                array.Print(array, lowerIndex);
+            }
+            else
+            {
+                Console.WriteLine("Введены некорректные индексы");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ homework_module08/RangeOfArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_module08
+{
+    class RangeOfArray
+    {
+        public int _start;
+        public int _end;
+        public int[] _arr;
+        public RangeOfArray(int end, int start)
+        {
+            _start = start;
+            _end = end;
+            _arr = new int[Math.Abs(_end - _start) + 1];
+        }
+        public void Print(RangeOfArray arr, int start)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                Console.WriteLine($"{i + start} элемент массива равен = " + _arr[i]);
+            }
+        }
+    }
+}
